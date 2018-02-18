383f7e6ba6bc5110943f38b9978556e9aea5907b
manojkumar3036 <shah.manoj86@gmail.com>
2018-02-18 21:29:36 +0530

    Storing user input to an array

---
+++ userinputarray.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# Author: Manoj Kumar
+
+# display a message
+
+echo "Enter N for number for inputs"
+read N
+for((i=1;i<=N;i++))
+	do
+		#read i # storing the value
+		# echo $i # displaying the value [it's working]
+		read -a array	
+	done
+
+# displaying the array which is created of N length
+
+
+
+
+
+
