cd8971b5e1d7f9d5c1d7c62c7e902aa16fe010fa
manojkumar3036 <shah.manoj86@gmail.com>
2018-02-18 19:50:37 +0530

    For loop to print 1 to 100 odd numbers

---
+++ Looping_Skipping.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# Script to display odd natural numbers from 1 to 99
+
+# Author: Manoj Kumar
+
+
+for i in {1..99..2} 
+# Here the first and second argument is the starting and ending of the loop. 3rd argument is the increment 
+	do 
+	echo $i
+
+	done
+
+
+
+
+
