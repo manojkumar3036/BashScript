afd65f601ffac63c768a45b25e12b1a35b5b2682
manojkumar3036 <shah.manoj86@gmail.com>
2018-02-19 13:58:25 +0530

    Computing the average of the numbers

---
+++ ComputeAverage.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+# Compute the Average of N numbers
+# Author: Manoj Kumar
+
+echo  'Enter the value of N:' 
+read N
+declare -a array # declaring the array
+declare sum=0
+# C style for loop
+for((i=1;i<=N;i++))
+	do	
+		read array[$i] # reading the elements of the array
+		#sum+=array[$i]
+		
+	done
+
+
+# displaying the elements of the array
+	echo 'Elements of the array:' ${array[*]} 
+
+
+for i in ${array[*]}
+	do
+	let sum+=$i	
+	done
+
+
+
+echo "Total" $sum 
+Average=`expr $sum/$N`
+echo $Average
+
+
+
+
+
+
+
+
+
+
+
+
+
