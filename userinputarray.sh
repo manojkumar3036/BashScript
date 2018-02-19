aa55266e3a1090791df9a966c669ca342b0fa566
manojkumar3036 <shah.manoj86@gmail.com>
2018-02-19 13:36:14 +0530

    Modified the for loop for reading array

--- userinputarray.sh
+++ userinputarray.sh
@@ -6,16 +6,19 @@
 
 echo "Enter N for number for inputs"
 read N
+declare -a array
 for((i=1;i<=N;i++))
 	do
 		#read i # storing the value
 		# echo $i # displaying the value [it's working]
-		read -a array	
-	done
+		read  array[$i]
+			done
 
 # displaying the array which is created of N length
-
-
+#for e in "${array[*]}"
+#do
+	echo ${array[*]}
+#done
 
 
 
